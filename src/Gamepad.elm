239e06da4978d931ad6652c8dfa54d6a71d83810
hoosierEE <lx@129-79-194-220.dhcp-bl.indiana.edu>
2016-01-27 13:32:23 -0500

    fix for current version of Elm compiler

--- src/Gamepad.elm
+++ src/Gamepad.elm
@@ -5,16 +5,24 @@ module Gamepad
   ) where
 
 {-| Library for working with gamepads
+# button
+@docs Button
+# gamepad
+@docs Gamepad
+# pads
+@docs gamepads
 -}
 
 import Native.Gamepad
 import Signal exposing (Signal)
 
+{-| Button -}
 type alias Button =
   { pressed : Bool
   , value : Float
   }
 
+{-| Gamepad -}
 type alias Gamepad =
   { id : String
   , axes : List Float
@@ -25,6 +33,7 @@ type alias Gamepad =
   -- timestamp
   }
 
+{-| gamepads -}
 gamepads : Signal (List Gamepad)
 gamepads =
   Native.Gamepad.gamepads
