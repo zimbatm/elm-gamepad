185721033c5303c065924b456b70c7002b7492d4
Conrad Parker <conrad@metadecks.org>
2016-03-04 04:56:15 +1100

    Update for Elm 0.16

--- Main.elm
+++ Main.elm
@@ -14,12 +14,12 @@ mario = { x=0, y=0, vx=0, vy=0, dir="right" }
 
 -- UPDATE -- ("m" is for Mario)
 --jump {y} m = if y > 0 && m.y == 0 then { m | vy <- 5 } else m
-jump {y} m = if y > 0 then { m | vy <- 5 } else m
-gravity t m = if m.y > 0 then { m | vy <- m.vy - t/4 } else m
-physics t m = { m | x <- m.x + t*m.vx , y <- max 0 (m.y + t*m.vy) }
-walk {x} m = { m | vx <- (toFloat x) * 2
-                 , dir <- if x < 0 then "left" else
-                          if x > 0 then "right" else m.dir }
+jump {y} m = if y > 0 then { m | vy = 5 } else m
+gravity t m = if m.y > 0 then { m | vy = m.vy - t/4 } else m
+physics t m = { m | x = m.x + t*m.vx , y = max 0 (m.y + t*m.vy) }
+walk {x} m = { m | vx = (toFloat x) * 2
+                 , dir = if x < 0 then "left" else
+                         if x > 0 then "right" else m.dir }
 
 step (dt, keys) =
   jump keys >> gravity dt >> walk keys >> physics dt
